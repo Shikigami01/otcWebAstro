26dbc8b31aa296fd0bec18e06f58ff7c1a3c60cd
houraijin <contact@overthecoffee.net>
2023-09-09 00:34:30 +0900

    feat: 🎸 add settings (storybook, lint, etc)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/_mixin.scss";`,
+        },
+      },
+    },
+  },
+});
