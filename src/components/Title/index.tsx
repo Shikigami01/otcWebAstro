4906f8e685f0edc0a245fac8532b431687e06b67
houraijin <contact@overthecoffee.net>
2023-09-11 23:58:01 +0900

    fix: 🐛 Title component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,10 +1,9 @@
-import type { ReactNode } from 'react';
 import { styles } from './styles';
 
 type Props = {
-  chidlren: ReactNode;
+  children: React.ReactNode;
 };
 
-export const Title: React.FC<Props> = ({ chidlren }) => {
-  return <h1 css={styles.heading}>{chidlren}</h1>;
+export const Title: React.FC<Props> = ({ children }) => {
+  return <h1 css={styles.heading}>{children}</h1>;
 };
--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -3,7 +3,7 @@ import { colors } from '../../utils/colors';
 
 export const styles = {
   heading: css`
-    font-size: 2rem;
+    font-size: 1.6rem;
     line-height: 1.5;
     font-weight: 700;
     border-bottom: 2px solid #040000;
