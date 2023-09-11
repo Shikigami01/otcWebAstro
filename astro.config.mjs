2eab46351fab6b1262c94cd0b1f3949c1b9c83e1
houraijin <contact@overthecoffee.net>
2023-09-11 23:56:51 +0900

    fix: 🐛 vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@import "src/styles/_mixin.scss";`,
-        },
-      },
-    },
-  },
 });
